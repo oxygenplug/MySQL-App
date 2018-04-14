d9962c66d64b77b26b38fce6b030a4b0e4bf14ed
oxygenplug <dan.garces2@gmail.com>
2018-04-14 18:37:54 -0400

    now validates inventory stock before purchases

--- nodeapp.js
+++ nodeapp.js
@@ -32,50 +32,60 @@ function displayItems() {
     runApp();
 
   })
- 
+
 }
 
 function runApp() {
   inquirer.prompt([{
 
-      name: 'product_selection',
-      type: 'input',
-      message: 'Please enter the id of the product you would like to buy',
+    name: 'product_selection',
+    type: 'input',
+    message: 'Please enter the id of the product you would like to buy',
   },
   {
-      name: 'quantity_selection',
-      type: 'input',
-      message: 'Please enter the quantity you would like to buy'
-      
+    name: 'quantity_selection',
+    type: 'input',
+    message: 'Please enter the quantity you would like to buy'
+
   }])
-  .then(answer => {
-    userQuantity = answer.quantity_selection;
-    var query = connection.query("SELECT price FROM products WHERE item_id='" + answer.product_selection +"'", function (err, res) {
-      if(err) throw err;
-    var price  = res[0].price;
-    console.log('\nYour total comes out to: $' + (price * userQuantity) + '\nThank You!\n');
-    })
-    function updateProduct() {
-      console.log("Updating product quantities...\n");
-      var query2 = connection.query("SELECT stock_quantity FROM products WHERE item_id='" + answer.product_selection +"'", function(err, res){
-        var itemStock = res[0].stock_quantity;
+    .then(answer => {
+      userQuantity = answer.quantity_selection;
+      var query = connection.query("SELECT price FROM products WHERE item_id='" + answer.product_selection + "'", function (err, res) {
+        if (err) throw err;
+        var price = res[0].price;
+        checkItemInventory();
+
       })
-      var query3 = connection.query(
-        "UPDATE products SET ? WHERE ?",
-        [
-          {
-            quantity: itemStock - answer.quantity_selection
-          },
-          {
-            item_id: answer.product_selection
+      function checkItemInventory() {
+        var query2 = connection.query("SELECT stock_quantity FROM products WHERE item_id='" + answer.product_selection + "'", function (err, res) {
+          var inStock;
+          var remainingInventory = res[0].stock_quantity;
+          if (err) throw err;
+          if (answer.quantity_selection > remainingInventory) {
+            console.log('Sorry! We do not have enough stock');
+            return;
+            inStock = false;
           }
-        ],
-        function (err, res) {
-          console.log(res)
+          else if (inStock = true) {
+            updateProduct();
+            console.log('\nYour total comes out to: $' + (price * userQuantity) + '\nThank You!\n');
+          }
         }
-      );
-    }
-    updateProduct();
-})
+        )
+      }
+      function updateProduct() {
+        console.log("Updating product quantities...\n");
+        var query3 = connection.query("SELECT stock_quantity FROM products WHERE item_id='" + answer.product_selection + "'", function (err, res) {
+          var itemStock = res[0].stock_quantity;
+        })
+
+        var amountPurchased = answer.quantity_selection
+        var itemId = answer.product_selection
+        var insert = ['products', 'stock_quantity', 'stock_quantity', amountPurchased, 'item_id', itemId]
+        var query4 = connection.query('UPDATE ?? SET ?? = ?? - ? WHERE ?? = ?', insert, function (error) {
+        });
+      }
+
+    })
 }
 
